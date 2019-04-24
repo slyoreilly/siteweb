bccc90a94edd4ad0026d5b9114dad76b29e00350
Sylvain O'Reilly (sur Helsinki) <soreilly@syncstats.com>
2019-04-24 10:29:48 -0400

    Printemps CosomMixte

---
+++ ligues/cosommixte/P2019/ajoutsjs.js
@@ -0,0 +1,55 @@
+
+saisonMardi = 245;
+saisonJeudi = 244;
+
+
+topLiensMardi = [{
+	valeur:"../index.html",
+	texte:"Accueil"
+},
+{
+	valeur:"mardi_cosom_montreal.html",
+	texte:"Mardi"
+},{
+	valeur:"nouvelles_cosom_montreal/2019/printemps_cosom_montreal/chroniques_mardi_cosom_montreal.html",
+	texte:"Chroniques"
+},{
+	valeur:"equipes_cosom_montreal/2019/printemps_cosom_montreal/equipes_mardi_cosom_montreal.html",
+	texte:"Équipes"
+},{
+	valeur:"horaires_cosom_montreal/2019/printemps_cosom_montreal/horaires_mardi_cosom_montreal.html",
+	texte:"Horaires"
+},{
+	valeur:"classements_cosom_montreal/2019/printemps_cosom_montreal/classements_mardi_cosom_montreal.html",
+	texte:"Classements"
+},{
+	valeur:"statistiques_cosom_montreal/2019/printemps_cosom_montreal/statistiques_mardi_cosom_montreal.html",
+	texte:"Compteurs"
+}
+];
+
+
+topLiensJeudi = [{
+	valeur:"../index.html",
+	texte:"Accueil"
+},
+{
+	valeur:"jeudi_cosom_montreal.html",
+	texte:"Jeudi"
+},{
+	valeur:"nouvelles_cosom_montreal/2019/printemps_cosom_montreal/chroniques_jeudi_cosom_montreal.html",
+	texte:"Chroniques"
+},{
+	valeur:"equipes_cosom_montreal/2019/printemps_cosom_montreal/equipes_jeudi_cosom_montreal.html",
+	texte:"Équipes"
+},{
+	valeur:"horaires_cosom_montreal/2019/printemps_cosom_montreal/horaires_jeudi_cosom_montreal.html",
+	texte:"Horaires"
+},{
+	valeur:"classements_cosom_montreal/2019/printemps_cosom_montreal/classements_jeudi_cosom_montreal.html",
+	texte:"Classements"
+},{
+	valeur:"statistiques_cosom_montreal/2019/printemps_cosom_montreal/statistiques_jeudi_cosom_montreal.html",
+	texte:"Compteurs"
+}
+];
