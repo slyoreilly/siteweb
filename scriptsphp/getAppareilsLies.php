1cc781848c72e57fa5f849302ee7c9e8e637ed2e
Sylvain O'Reilly (sur Helsinki) <soreilly@syncstats.com>
2020-08-04 15:48:27 -0400

    Mineur dans gAL

--- scriptsphp/getAppareilsLies.php
+++ scriptsphp/getAppareilsLies.php
@@ -43,14 +43,14 @@ mysqli_query($conn,"SET CHARACTER SET 'utf8'");
 		$cptCams =0;
 		while($rangSel = mysqli_fetch_assoc($resultSelCam)){
 		$cams[] = $rangSel;	
-			$cams[$cptCams]->memoire=round($rangSel['memoire']/1000000);
-			$cams[$cptCams]->camId=$rangSel['camId'];
+			$cams[$cptCams]['memoire']=round($rangSel['memoire']/1000000);
+			$cams[$cptCams]['camId']=$rangSel['camId'];
 			if((time()-strtotime($rangSel['dernierMaJ']))>3600&&($rangSel['codeEtat']==10)){
-				$cams[$cptCams]->codeEtat='30';
+				$cams[$cptCams]['codeEtat']='30';
 				
 			
 				if((time()-strtotime($rangSel['dernierMaJ']))>600&&($rangSel['codeEtat']==10)){
-					$cams[$cptCams]->codeEtat='20';
+					$cams[$cptCams]['codeEtat']='20';
 				}	
 			}	
 			$cptCams++;
