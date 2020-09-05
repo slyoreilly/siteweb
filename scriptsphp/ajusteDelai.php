3ec500dd2501ad9a1f55db43ea9fe5c1ddd3e38c
Sylvain O'Reilly (sur Helsinki) <soreilly@syncstats.com>
2020-09-05 13:20:57 -0400

    verbose dans ajusteDelai

--- scriptsphp/ajusteDelai.php
+++ scriptsphp/ajusteDelai.php
@@ -42,24 +42,24 @@ $delai=$_POST["delai"];
 
 $qSel="SELECT * FROM StatutCam 
 		 WHERE camId='{$cam}'  LIMIT 1";
-$resCam=mysqli_query($conn,$qSel) or die(mysqli_error($conn).' damn');
+$resCam=mysqli_query($conn,$qSel) or die(mysqli_error($conn).$qSel.' damn');
 while($rangCam=mysqli_fetch_array($resCam)){
 
 
 	$qCon="SELECT * FROM Controle 
 	WHERE telId='{$rangCam['telId']}' AND arg0='videos' AND arg1='recut' AND arg2= '{$fic}' and etatSync='3' ";
 
-	$resCon=mysqli_query($conn,$qCon) or die(mysqli_error($conn).' damn');
+	$resCon=mysqli_query($conn,$qCon) or die(mysqli_error($conn).$qCon.' damn');
 	if(mysqli_num_rows($resCon)>0){
 		while($rangCon=mysqli_fetch_array($resCon)){
 			$queryMod = "UPDATE Controle SET valeur = '{$delai}'
 							WHERE  telId='{$rangCam['telId']}' AND arg0='videos' AND arg1='recut' AND arg2= '{$fic}' and etatSync=3";
-			mysqli_query($conn,$queryMod) or die("Erreur: "+$queryMod+"\n"+mysqli_error($conn));
+			mysqli_query($conn,$queryMod) or die("Erreur: ".$queryMod."\n".mysqli_error($conn));
 		}
 	}else{
-
-		$ajouteBut =mysqli_query($conn,"INSERT INTO Controle (`telId`, `arg0`, `arg1`, `arg2`, `valeur`, `cleValeur`, `etatSync`) 
-		VALUES ('{$rangCam['telId']}','videos','recut','{$fic}','{$delai}',NULL, 3)");
+		$qAjoute="INSERT INTO Controle (`telId`, `arg0`, `arg1`, `arg2`, `valeur`, `cleValeur`, `etatSync`) 
+		VALUES ('{$rangCam['telId']}','videos','recut','{$fic}','{$delai}',NULL, 3)";
+		$ajouteBut =mysqli_query($conn,$qAjoute)or die("Erreur: ".$qAjoute."\n".mysqli_error($conn));
 
 	}
 
