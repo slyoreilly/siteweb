b37ec7fd8e058365264180f5e6fc29dc8376fac6
Sylvain O'Reilly (sur Helsinki) <soreilly@syncstats.com>
2020-09-24 15:51:45 -0400

    ressaie avec =>

--- syncscript/dechargeMatchs_2.php
+++ syncscript/dechargeMatchs_2.php
@@ -74,10 +74,10 @@ foreach ($leMatch as $evenement) {
 
 					// use key 'http' even if you send the request to https://...
 					$options = array(
-    					'http' -> array(
-        				'header'  -> "Content-type: application/x-www-form-urlencoded\r\n",
-        				'method'  -> 'POST',
-        				'content' -> http_build_query($data)
+    					'http' => array(
+        				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        				'method'  => 'POST',
+        				'content' => http_build_query($data)
     					)
 					);
 					$context  = stream_context_create($options);
