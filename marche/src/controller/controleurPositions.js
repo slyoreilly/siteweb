683b3b126fa8c86a4c86e0cd32d0d9cbd3d1f1e0
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-11 12:22:24 -0500

    retentative de push master

--- marche/src/controller/controleurPositions.js
+++
@@ -1,5 +0,0 @@
- app.controller('controleurPositions',function($scope,positionProvider){
-    	$scope.positions = positionProvider.getPositions();
-    	
-    	$scope.ajouterPosition = function(p){positionProvider.creer(p);};
-    });
