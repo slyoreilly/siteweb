90d26e3072975fe7feafde60802672d9920437b3
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-15 18:50:24 -0400

    hrthtrh

--- syncscript/uploadAppVersion.php
+++ syncscript/uploadAppVersion.php
@@ -15,7 +15,7 @@ $headers = apache_request_headers();
 
 if(strcasecmp($headers['confirmation-key'],'z0d2N9IvAifAoZZgVqDfGDB2QGakzuo9pmb0MOIf')){
 	error_log("ecriture de l'image: header non valide. On se fait hacker? ");
-
+    error_log("HTTP/1.1 403 Forbidden");	
 }else{
 
     if($_FILES['fichier']['size'] > 0)
