93a0c7eb84db6b62f34107e2d77d9d77a09629cf
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-16 11:33:25 -0400

    ghmgmghm

--- syncscript/setAppVersion.php
+++ syncscript/setAppVersion.php
@@ -1,13 +1,12 @@
 
 <?php
 require '../scriptsphp/defenvvar.php';
-http_response_code(500);
-header('http/1.0 404 not found');
-echo 'a';
+
+
 $OK = true;
 $versionName="";
 $channel ="alpha";
-$isActive = false;
+$isActive = 0;
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
@@ -21,30 +20,6 @@ $versionCode=$data->versionCode;
 $versionName=$data->versionName;
 $isActive =$data->isActive;
 
-
-
-/*
-if(isset($_POST['app'])){
-    $app = $_POST['app'];
-}else{
-    $OK = false;
-}
-
-if(isset($_POST['channel'])){
-    $channel = $_POST['channel'];
-}
-if(isset($_POST['versionCode'])){
-    $versionCode = $_POST['versionCode'];
-}else{
-    $OK = false;
-}
-if(isset($_POST['versionName'])){
-    $versionName = $_POST['versionName'];
-}
-if(isset($_POST['isActive'])){
-    $isActive = $_POST['isActive'];
-}*/
-echo 'b';
 
 if($OK){
 
@@ -68,12 +43,11 @@ $qGet ="SELECT systemconfigId
              " ;
     $res = mysqli_query($conn,$qGet) or die("Erreur dans le get de set AppVersion "."\n".mysqli_error($conn));
 
-    echo "qGet: ".$qGet." num_rows: ".mysqli_num_rows($res);
     if(mysqli_num_rows($res)==0){
-        echo 'd';
+
         $qSet = "INSERT INTO SystemConfig (app, channel, versionCode, versionName, isActive, lastUpdate) 
                 VALUES ('{$app}','{$channel}','{$versionCode}','{$versionName}','{$isActive}',NOW())";
-                echo 'e';
+  
     }else{
 
         $qSet = "UPDATE SystemConfig 
@@ -84,29 +58,26 @@ $qGet ="SELECT systemconfigId
             AND versionName='{$versionName}'";
 
     }
-    echo 'a';
+
     $res =false;
 
     $res = mysqli_query($conn,$qSet) or die("Erreur dans le set de set AppVersion "."\n".mysqli_error($conn));
-error_log($qSet);
+
 if($res=false){
     http_response_code(409);
 }else{http_response_code(200);}
     
-echo 'a';
+
 
 
 mysqli_close($conn);
 }
 
 else{
-    echo 'c';
-
     http_response_code(412);
 }
 
-http_response_code(500);
-header('http/1.0 404 not found');
+
 
 ?>
 
