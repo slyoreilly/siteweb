4f7ecc45d7eadee9bd9182220c8b352f7dd6cbb5
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-16 12:22:26 -0400

    rthtrht

--- syncscript/detectAppChange.php
+++ syncscript/detectAppChange.php
@@ -1,30 +1,49 @@
 
 <?php
 require '../scriptsphp/defenvvar.php';
-$tableEq = 'TableEquipe';
-$tableLigue = 'Ligue';
-$tableMatch = 'TableMatch';
-$tableEvent = 'TableEvenement0';
-$tableJoueur = 'TableJoueur';
-$tableAbon = 'AbonnementLigue';
-$tableUser = 'TableUser';
+
+    // Create connection
+    $conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    mysqli_query($conn,"SET NAMES 'utf8'");
+    mysqli_query($conn,"SET CHARACTER SET 'utf8'"); 
+    
+    $qGet ="SELECT systemconfigId 
+            FROM SystemConfig 
+            WHERE app='{$app}'
+                AND channel='{$channel}'
+                AND versionCode='{$versionCode}'
+                AND versionName='{$versionName}'
+                 " ;
 
 $app = $_POST['app'];
 $channel =$_POST['channel'];
-$version =$_POST['version'];
-error_log("test0: ".$channel." ".$app." ".$version);
+$versionCode =$_POST['version'];
 
-$mVersionArray= array("synccam"=>array("stable"=>"95","beta"=>"95","alpha"=>"95"),"syncstats"=>array("stable"=>"64","beta"=>"65","alpha"=>"65"));
-error_log("test-1: ".json_encode($mVersionArray));
 
-error_log("test: ".$mVersionArray[$app][$channel]);
 
-    if($mVersionArray[$app][$channel]>$version)
-    {
-        echo "http://syncstats.com/android/".$app."/".$channel."/".$app.".apk";
-        error_log("test2: "."http://syncstats.com/android/".$app."/".$channel."/".$app.".apk");
+$qGet ="SELECT systemconfigId 
+        FROM SystemConfig 
+        WHERE app='{$app}'
+            AND channel='{$channel}'
+            AND versionCode>'{$versionCode}'
+             " ;
+    $res = mysqli_query($conn,$qGet) or die("Erreur dans le get de set AppVersion "."\n".mysqli_error($conn));
+
+    if(mysqli_num_rows($res)>0){
+        if(!strcmp($channel,"stable")){
+            echo "http://syncstats.com/android/".$app."/".$channel."/".$app.".apk";
+        }
+        else{
+            echo "http://vieuxsite.sm.syncstats.ca/android/".$app."/".$channel."/".$app.".apk";
+        }
+    }else{
+        echo "";
     }
-    else echo "";
 
 ?>
 
