2acac0ddea40204068b9a8a9d0392a900e73a214
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-16 09:56:04 -0400

    fgjjgf

--- syncscript/setAppVersion2.php
+++ syncscript/setAppVersion2.php
@@ -75,7 +75,7 @@ $qGet ="SELECT systemconfigId
         WHERE  app='{$app}'
             AND channel='{$channel}'
             AND versionCode='{$versionCode}'
-            AND versionName='{$versionName}'"
+            AND versionName='{$versionName}'";
 
     }
 
