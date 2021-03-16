c3fbee79d66013c4fd88d90a9eee3325fc887fac
Sylvain O'Reilly <soreilly@syncstats.com>
2021-03-16 09:43:35 -0400

    fgngfnfg

---
+++ syncscript/setAppVersion2.php
@@ -0,0 +1,97 @@
+
+<?php
+require '/var/www/html/scriptsphp/defenvvar.php';
+
+
+$OK = true;
+$versionName="";
+$channel ="alpha";
+$isActive = false;
+
+$opts = getopt('',['app:','channel','versionCode:','versionName:','isaActive');
+if ($val !== false) {
+	echo var_export($val, true);
+}
+else {
+	echo "Could not get value of command line option\n";
+}
+
+
+//////////////////////////////////////////////
+
+
+
+if(isset($opts['app'])){
+    $app = $opts['app'];
+}else{
+    $OK = false;
+}
+
+if(isset($opts['channel'])){
+    $channel = $opts['channel'];
+}
+if(isset($opts['versionCode'])){
+    $versionCode = $opts['versionCode'];
+}else{
+    $OK = false;
+}
+if(isset($opts['versionName'])){
+    $versionName = $opts['versionName'];
+}
+if(isset($opts['isActive'])){
+    $isActive = $opts['isActive'];
+}
+
+
+if($OK){
+
+
+    // Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn,"SET NAMES 'utf8'");
+mysqli_query($conn,"SET CHARACTER SET 'utf8'"); 
+
+$qGet ="SELECT systemconfigId 
+        FROM SystemConfig 
+        WHERE app='{$app}'
+            AND channel='{$channel}'
+            AND versionCode='{$versionCode}'
+            AND versionName='{$versionName}'
+             " ;
+    $res = mysqli_query($conn,$qGet) or die("Erreur dans le get de set AppVersion "+"\n"+mysqli_error($conn));
+
+    if(mysqli_num_rows($res)==0){
+        $qSet = "INSERT INTO SystemConfig (app, channel, versionCode, versionName, isActive, lastUpdate) 
+                VALUES ('{$app}','{$channel}','{$versionCode}','{$versionName}','{$isActive}',NOW())";
+    }else{
+
+        $qSet = "UPDATE SystemConfig 
+        SET app='{$app}', channel='{$channel}', versionCode='{$versionCode}', versionName='{$versionName}', isActive='{$isActive}'
+        WHERE  app='{$app}'
+            AND channel='{$channel}'
+            AND versionCode='{$versionCode}'
+            AND versionName='{$versionName}'"
+
+    }
+
+    $res = mysqli_query($conn,$qSet) or die("Erreur dans le set de set AppVersion "+"\n"+mysqli_error($conn));
+    http_response_code(200);
+
+
+mysqli_close($conn);
+}
+
+else{
+
+    http_response_code(412);
+}
+
+
+
+?>
+
