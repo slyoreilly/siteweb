b044dcdc8f4878d03cb8463d7086a8c39c6c9831
Sylvain O'Reilly <soreilly@syncstats.com>
2021-06-29 14:22:10 -0400

    ajout généric

--- syncscript/dechargeMatchs_2.php
+++ syncscript/dechargeMatchs_2.php
@@ -848,6 +848,58 @@ foreach ($leMatch as $evenement) {
 		}
 	}
 
+	if (!strcmp($evenement['type'],'generic')) {
+		$matchAEnr = parseMatchID($evenement['match_id']);
+		if (isset($matchAEnr['ligueId'])) {$ligueId = $matchAEnr['ligueId'];
+		}
+		if (isset($evenement['noseq'])) {$noseq = $evenement['noseq'];
+		}else{$noseq=0;}
+		
+		$trouvePun=0;
+
+		$retPun = $evenement['chrono'];
+		// retourner le but, sans correction de chrono.
+
+		if (isset($heure)) {$evenement['chrono'] = $evenement['chrono'] + $heureServeur - $heure;
+		}
+		switch(f_es($evenement['es'])) {
+
+			case 10 :
+				$qDel = "DELETE FROM TableEvenement0 WHERE chrono='{$evenement['chrono']}' AND match_event_id='{$evenement['match_id']}'";
+				mysqli_query($conn,$qDel) or die(mysqli_error($conn) . $qDel);
+			//	break;		 NO BREAK!!!!!!!
+			case 15 :
+			
+				$qSelPun = "SELECT event_id FROM TableEvenement0 WHERE match_event_id='{$evenement['match_id']}' AND code='{$evenement['code']}' AND noSequence={$evenement['noseq']}";
+				$resPun = mysqli_query($conn,$qSelPun) or die(mysqli_error($conn) . $qSelPun);
+				$trouvePun = mysqli_num_rows($resPun);
+				mysqli_data_seek($resPun,0);
+				$punId = mysqli_fetch_row($resPun);
+
+			//	break;		 NO BREAK!!!!!!!
+			case 12 :
+				if ($trouvePun == 0) {
+				$qInsM = "INSERT INTO TableEvenement0 (match_event_id, equipe_event_id,joueur_event_ref,chrono,souscode,code,noSequence) VALUES ('{$evenement['match_id']}','{$evenement['eqId']}','{$evenement['joueur']}','{$evenement['chrono']}','{$evenement['sc']}','{$evenement['code']}','{$noseq}')";
+
+				mysqli_query($conn,$qInsM) or die(mysqli_error($conn) . $qInsM);
+				$retObj = array("type"=>"generic","chronoInit"=>$retPun,"chronoFin"=>$evenement['chrono'],"db_id"=>"{$evenement['db_id']}");
+				array_push($syncOKdetail, $retObj);
+				
+				} else{
+					array_push($syncOK, $retPun);
+					$retObj = array("type"=>"punition","chronoInit"=>$retPun,"chronoFin"=>$evenement['chrono'],"db_id"=>"{$evenement['db_id']}");
+					array_push($syncOKdetail, $retObj);
+
+				}
+
+
+		}
+				
+
+
+
+	}
+
 	$IJ++;
 	//echo json_encode($syncOK);
 }
