63c27f8ab0b2a37d0cee3cddf9d1d8796e837e61
soreilly@syncstats.com <soreilly@syncstats.com>
2022-03-16 11:32:13 -0400

    dgfdg

--- stats2/setBatchAVenir.php
+++ stats2/setBatchAVenir.php
@@ -35,8 +35,9 @@ $dateFin = $unMatch['dateFin'];
 $ligueId = $unMatch['ligueId'];
 $arenaId = $unMatch['arenaId'];
 $arbitreId = $unMatch['arbitreId'];
-$appareils_json = $unMatch['appareils'];
-$appareils = json_decode($appareils_json, true);
+$appareils = $unMatch['appareils'];
+//$appareils_json = $unMatch['appareils'];
+//$appareils = json_decode($appareils_json, true);
 
 //$matchId = substr($dateDeb, 0, 4) . "/" . substr($dateDeb, 5, 2) . "/" . substr($dateDeb, 8, 2) . "_" . $eqDom . "_" . $eqVis;
 
@@ -161,8 +162,8 @@ $matchId = substr($dateDeb, 0, 4) . "/" . substr($dateDeb, 5, 2) . "/" . substr(
 $qQuery ="INSERT INTO TableMatch (matchId, matchIdRef, mavId, alignementDom, alignementVis, gardienDom, gardienVis, eq_dom, eq_vis, date, dateFin, ligueRef,dernierMAJ,arenaId,arbitreId) 
 VALUES ('{$matchId}','{$matchId}','{$mavId}','{$jDom}', '{$jVis}','{$gDom}','{$gVis}','{$eqDom}','{$eqVis}','{$dateDeb}','{$dateFin}','{$ligueId}',NOW(),'{$arenaId}','{$arbitreId}')";
 echo $qQuery;
-//$rTM = mysqli_query($conn, $qQuery) or die(mysqli_error($conn) . " INSERT INTO TableMatch");
-//	$match_id = mysqli_insert_id($conn);
+$rTM = mysqli_query($conn, $qQuery) or die(mysqli_error($conn) . " INSERT INTO TableMatch");
+	$match_id = mysqli_insert_id($conn);
 
 /*
 if ($appareils != null) {
