e6ef10feefc103b30716f59e5f68a3087dd2f303
soreilly@syncstats.com <soreilly@syncstats.com>
2022-08-25 16:25:16 -0400

    Nouvelle section api

---
+++ syncscript/api/upsertClips.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR . "syncstatsconfig.php");
+require("../scriptsphp/calculeMatch2.php");  /// N'appelle rien, défini seulement la fonction
+											 /// CalculeMatch(ligueId);
+
+require '../scriptsphp/defenvvar.php';
+// Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn,"SET NAMES 'utf8'");
+mysqli_query($conn,"SET CHARACTER SET 'utf8'");
+	
+$clips = $_POST['clips'];
+$heure = $_POST['heure'];
+$heureServeur = time()*1000;
+
+$syncOK = array();
+
+foreach ($clips as $unClip) {
+
+
+		if (isset($heure)) {
+			$retClip = $evenement['chrono'];
+			// retourner le but, sans correction de chrono.
+			$unClip['chrono'] = $unClip['chrono'] + $heureServeur - $heure;
+		}
+
+
+			
+
+				$qInsM = "INSERT INTO Clips (matchId, chrono, scoringEnd, type) VALUES ('{$unClip['matchLongId']}','{$unClip['chrono']}',Null,5)";
+
+				mysqli_query($conn,$qInsM) or die(mysqli_error($conn) . $qInsM);
+				$webIdClip=mysqli_insert_id($conn);
+				
+
+				
+				array_push($syncOK, $retClip);
+				$retObj = array("id"=>$unClip["id"],"SyncKey"=>$webIdClip);
+		
+	}
+
+echo json_encode($syncOK);
+
+
+mysqli_close($conn);
+
+?>
