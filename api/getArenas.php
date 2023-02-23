5d3e6336b7ff3261f8a0f740302b7396871521be
soreilly@syncstats.com <soreilly@syncstats.com>
2023-02-23 16:51:00 -0500

    Go!

---
+++ api/getArenas.php
@@ -0,0 +1,76 @@
+<?php 
+require '../scriptsphp/defenvvar.php';
+
+$username = null;
+
+if(isset($_POST['username'])){
+	$username =$_POST['username'];
+}
+
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn, "SET NAMES 'utf8'");
+mysqli_query($conn, "SET CHARACTER SET 'utf8'");
+mysqli_set_charset($conn, "utf8");
+mysqli_query($conn, "SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY','')");
+
+function utf8ize($mixed) {
+    if (is_array($mixed)) {
+        foreach ($mixed as $key => $value) {
+            $mixed[$key] = utf8ize($value);
+        }
+    } else if (is_string ($mixed)) {
+        return utf8_encode($mixed);
+    }
+    return $mixed;
+}
+$strRetour = $arenaId;
+if (($username !== 0) && ($username != null)) {
+
+
+	$retour = mysqli_query($conn, "SELECT 
+		 TableArena.arenaId,
+		 TableArena.nomArena,
+		 TableArena.nomGlace
+						FROM TableUser
+						LEFT JOIN AbonnementLigue
+							ON (TableUser.noCompte=AbonnementLigue.userid)
+						INNER JOIN abonLigueArena
+							ON (AbonnementLigue.ligueid=abonLigueArena.ligueId)
+						LEFT JOIN Gabarits
+							ON (abonLigueArena.gabaritId= Gabarits.gabaritId)
+						LEFT JOIN TableArena
+							ON (abonLigueArena.arenaId=TableArena.arenaId)
+						WHERE username='{$username}' 
+							AND abonLigueArena.finAbon>Now() 
+							AND abonLigueArena.debutAbon<Now()  
+						group by TableArena.arenaId
+						 	") or die(mysqli_error($conn));
+							
+
+	$lesArenas = array();
+
+
+	while ($r = mysqli_fetch_assoc($retour)) {
+		$unArena=array();
+		$unArena['typeSurface'] = $r['type'];
+		$unArena['plateauId'] = $r['arenaId'];
+		$unArena['nomEmplacement'] = $r['nomArena'];
+		$unArena['nomSurface'] = $r['nomGlace'];
+		array_push($lesArenas,$unArena);
+	}
+	
+	echo json_encode($lesArenas);
+							
+							
+							
+							
+} 
+mysqli_close($conn);
+		//header("HTTP/1.1 200 OK");
+?>
+
