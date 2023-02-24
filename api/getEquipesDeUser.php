2f6aeb9136a8b352601aaeacaadf201544cb8734
soreilly@syncstats.com <soreilly@syncstats.com>
2023-02-24 14:21:18 -0500

    avec Equipes

---
+++ api/getEquipesDeUser.php
@@ -0,0 +1,68 @@
+<?php
+////////////////////////////////////////////////////////////
+//
+//	getEquipesDeUser.php
+//	Est appellé dans equipeRepository.kt
+//
+//
+////////////////////////////////////////////////////////////
+
+
+require '../scriptsphp/defenvvar.php';
+
+
+
+////////////////////////////////////////////////////////////
+//
+// 	Connections � la base de donn�es
+//
+////////////////////////////////////////////////////////////
+
+
+// Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error($conn));
+}
+
+mysqli_query($conn,"SET NAMES 'utf8'");
+mysqli_query($conn,"SET CHARACTER SET 'utf8'");
+	
+$username = $_POST['username'];
+
+$qString = "SELECT TableEquipe.* 
+			FROM TableEquipe 
+			JOIN abonEquipeLigue 
+				ON abonEquipeLigue.equipeId = TableEquipe.equipe_id
+			JOIN AbonnementLigue 
+				ON AbonnementLigue.ligueId = abonEquipeLigue.ligueId 
+			JOIN TableUser 
+				ON AbonnementLigue.userId = TableUser.noCompte 
+			WHERE TableUser.username = '{$username}' 			
+			GROUP BY equipe_id";
+
+
+$retour = mysqli_query($conn,$qString) or die(mysqli_error($conn));	
+//$strRetour.= mysql_num_rows($retour);
+//$strRetour.="rege";
+
+$vecEquipes = array();
+$Im=0;
+while($r = mysqli_fetch_array($retour,MYSQLI_ASSOC)) {
+	$uneEquipe= array();
+	$uneEquipe['equipeId']=$r['equipeId'];
+	$uneEquipe['ligueId']=$r['ligueId'];
+	$uneEquipe['nom']=$r['nom_equipe'];
+	$uneEquipe['couleur1']=$r['couleur1'];
+	$uneEquipe['dernierMAJ']=$r['dernierMAJ'];
+	$uneEquipe['cleValeur']=$r['cleValeur'];
+	array_push($vecEquipes,$uneEquipe);
+}
+mysqli_close($conn);
+
+echo json_encode($vecEquipes);
+	
+
+
+?>
