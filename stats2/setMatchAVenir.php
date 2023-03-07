210f932271cf84a955981d51773f0c79f18f1d19
soreilly@syncstats.com <soreilly@syncstats.com>
2023-03-07 11:03:23 -0500

    date3

--- stats2/setMatchAVenir.php
+++ stats2/setMatchAVenir.php
@@ -125,7 +125,8 @@ function setPresences($connGA,$matchId,$alignement,$domVis)
 			// Préparation de la requête
 			$stmt = $connGA->prepare($sql);
 			$createur = "syncstats.com";
-			$maDate = date('Y-m-d H:i:s', time());
+			$maDate = (new DateTime())->format('Y-m-d H:i:s');
+
 			$stmt->bind_param("iiiiiiii", $joueur['joueurId'], $matchId, $domVis,$joueur['position'],$joueur['numero'],$joueur['statut'],$maDate, $createur);
 		
 			// Exécution de la requête
