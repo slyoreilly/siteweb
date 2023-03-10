71bd9d34833fa611c44e334c2e103f84c3137d7b
soreilly@syncstats.com <soreilly@syncstats.com>
2023-03-10 13:39:05 -0500

    EquipeCompose

--- api/getEquipesDeUser.php
+++ api/getEquipesDeUser.php
@@ -2,7 +2,7 @@
 ////////////////////////////////////////////////////////////
 //
 //	getEquipesDeUser.php
-//	Est appellé dans equipeRepository.kt
+//	Est appelé dans equipeRepository.kt
 //
 //
 ////////////////////////////////////////////////////////////
@@ -11,10 +11,9 @@
 require '../scriptsphp/defenvvar.php';
 
 
-
 ////////////////////////////////////////////////////////////
 //
-// 	Connections � la base de donn�es
+// 	Connection à la base de données
 //
 ////////////////////////////////////////////////////////////
 
@@ -26,46 +25,75 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error($conn));
 }
 
-mysqli_query($conn,"SET NAMES 'utf8'");
-mysqli_query($conn,"SET CHARACTER SET 'utf8'");
-	
+mysqli_query($conn, "SET NAMES 'utf8'");
+mysqli_query($conn, "SET CHARACTER SET 'utf8'");
+
 $username = $_POST['username'];
 
-$qString = "SELECT TableEquipe.* 
-			FROM TableEquipe 
-			JOIN abonEquipeLigue 
-				ON abonEquipeLigue.equipeId = TableEquipe.equipe_id
-			JOIN AbonnementLigue 
-				ON AbonnementLigue.ligueId = abonEquipeLigue.ligueId 
-			JOIN TableUser 
-				ON AbonnementLigue.userId = TableUser.noCompte 
-			WHERE TableUser.username = '{$username}' 			
-			GROUP BY equipe_id";
+$qString = "SELECT TableEquipe.*, abonJoueurEquipe.joueurId 
+            FROM TableEquipe 
+            JOIN abonEquipeLigue 
+                ON abonEquipeLigue.equipeId = TableEquipe.equipe_id
+            JOIN AbonnementLigue 
+                ON AbonnementLigue.ligueId = abonEquipeLigue.ligueId 
+            JOIN TableUser 
+                ON AbonnementLigue.userId = TableUser.noCompte 
+            JOIN abonJoueurEquipe 
+                ON abonJoueurEquipe.equipeId = TableEquipe.equipe_id
+            WHERE TableUser.username = '{$username}'
+			
+            GROUP BY equipe_id";
 
 
-$retour = mysqli_query($conn,$qString) or die(mysqli_error($conn));	
-//$strRetour.= mysql_num_rows($retour);
-//$strRetour.="rege";
+$retour = mysqli_query($conn, $qString) or die(mysqli_error($conn));
 
 $vecEquipes = array();
-$Im=0;
-while($r = mysqli_fetch_array($retour,MYSQLI_ASSOC)) {
-	$uneEquipe= array();
-	$uneEquipe['equipeId']=$r['equipe_id'];
-	$uneEquipe['ligueId']=$r['ligue_equipe_ref'];
-	$uneEquipe['nom']=$r['nom_equipe'];
-	$uneEquipe['couleur1']=$r['couleur1'];
-	if ($uneEquipe['couleur1'] === null) { // Si la valeur est nulle
-		$uneEquipe['couleur1'] = 'FFFFFF'; // On affecte la valeur par défaut
+
+while ($r = mysqli_fetch_array($retour, MYSQLI_ASSOC)) {
+    $uneEquipe = array();
+    $uneEquipe['equipeId'] = $r['equipe_id'];
+    $uneEquipe['ligueId'] = $r['ligue_equipe_ref'];
+    $uneEquipe['nom'] = $r['nom_equipe'];
+    $uneEquipe['couleur1'] = $r['couleur1'];
+    if ($uneEquipe['couleur1'] === null) { // Si la valeur est nulle
+        $uneEquipe['couleur1'] = 'FFFFFF'; // On affecte la valeur par défaut
+    }
+    $uneEquipe['dernierMAJ'] = strtotime($r['dernierMAJ']) * 1000;
+    $uneEquipe['cleValeur'] = $r['cleValeur'];
+    
+    // Recherche des joueurs de l'équipe
+    $qJoueurs = "SELECT TableJoueur.* 
+	FROM TableJoueur 
+	JOIN abonJoueurEquipe 
+	ON abonJoueurEquipe.joueurId = TableJoueur.joueur_id 
+		WHERE abonJoueurEquipe.equipeId = '{$uneEquipe['equipeId']}'
+		AND abonJoueurEquipe.debutAbon < Now()
+			AND abonJoueurEquipe.finAbon > Now()";
+    $retourJoueurs = mysqli_query($conn, $qJoueurs) or die(mysqli_error($conn));
+    $joueurs = array();
+    while ($j = mysqli_fetch_array($retourJoueurs, MYSQLI_ASSOC)) {
+        $joueur = array();
+        $joueur['SyncKey'] = $j['joueur_id'];
+        $joueur['nom'] = $j['NomJoueur'];
+		if($j['position']=='g' OR $j['position']=='g'){
+			$joueur['positionId'] = 5
+		}else{
+			$joueur['positionId'] = 1
+		}
+        $joueur['dernierMAJ'] = NOW();
+        $joueur['cleValeur'] = $j['cleValeur'];
+        array_push($joueurs,$joueur);
 	}
-	$uneEquipe['dernierMAJ']=strtotime($r['dernierMAJ'])*1000;
-	$uneEquipe['cleValeur']=$r['cleValeur'];
-	array_push($vecEquipes,$uneEquipe);
+    $uneEquipeCompose = array(
+        'equipe' => $uneEquipe,
+        'joueurs' => $joueurs
+    );
+	array_push($vecEquipes, $uneEquipeCompose);
 }
-mysqli_close($conn);
 
 echo json_encode($vecEquipes);
-	
 
+// Close connection
+mysqli_close($conn);
 
 ?>
