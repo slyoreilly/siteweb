719487fa3c92db2502855419b030d77cf474c166
soreilly@syncstats.com <soreilly@syncstats.com>
2023-04-05 14:03:15 -0400

    s

---
+++ api/getCamActionTemplate.php
@@ -0,0 +1,91 @@
+<?php
+
+
+/////////////////////////////////////////////////////////////
+//
+//  D�finitions des variables
+// 
+////////////////////////////////////////////////////////////
+
+require '../scriptsphp/defenvvar.php';
+
+
+$eventTypeIdArray=array();
+if(isset($_POST['eventTypeIds'])){
+$eventTypeIdArray = json_decode($_POST["eventTypeIds"]);
+//$eventTypeIdArray = json_decode($eventTypeId);
+}
+$leagueArray=array();
+if(isset($_POST['leagueIds'])){
+$leagueArray = json_decode($_POST["leagueIds"]);
+//$leagueArray = json_decode($leagueId);
+}
+////////////////////////////////////////////////////////////
+//
+// 	Connections � la base de donn�es
+//
+////////////////////////////////////////////////////////////
+
+// Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn, "SET NAMES 'utf8'");
+mysqli_query($conn, "SET CHARACTER SET 'utf8'");
+
+$saisonId =null;
+
+/////////////////////////////////////////////////////////////
+// 
+//
+
+$eventTypeString = "";
+	foreach($eventTypeIdArray as $s){
+		$eventTypeString  = $eventTypeString . "EventTypeId = '{$s}' OR ";
+	}
+	if(strlen($eventTypeString)>4){
+		$eventTypeString= substr($eventTypeString, 0, -3);
+	}
+	
+
+
+$leagueString = "";
+	foreach($leagueArray as $s){
+		$leagueString  = $leagueString . "LeagueId = '{$s}' OR ";
+	}
+	if(strlen($leagueString)>4){
+		$leagueString= substr($leagueString, 0, -3);
+	}
+	
+	$camActionTemplate=null;
+
+if(count($leagueArray)==0){
+	$secondProp = "";
+	if(!empty($eventTypeString)){
+			$secondProp ="AND  ({$eventTypeString})";}
+
+	$qCAT = "SELECT * FROM CamActionTemplate WHERE LeagueId = 0 {$secondProp}";
+	$rfCAT = mysqli_query($conn, $qCAT)
+	or die(mysqli_error($conn)." Select EventType leagueArray null".$qCAT." ||| ".$eventTypeIdArray); 
+}else{
+	$qCAT = "SELECT * FROM CamActionTemplate WHERE (LeagueId =0 OR {$leagueString}) AND ({$eventTypeString})";
+	$rfCAT = mysqli_query($conn, $qCAT)
+	or die(mysqli_error($conn)." Select EventType leagueArray not null :".$qCAT." ||| ".$eventTypeIdArray." ||| ".$leagueArray); 
+}
+
+$camActionTemplate= Array();
+
+while($rangeeCAT=mysqli_fetch_array($rfCAT))
+{
+	$camActionTemplate[] = $rangeeCAT;
+	
+}
+
+echo json_encode($camActionTemplate);
+	
+mysqli_close($conn);
+
+?>
---
+++ api/getEventType.php
@@ -0,0 +1,72 @@
+<?php
+
+
+/////////////////////////////////////////////////////////////
+//
+//  D�finitions des variables
+// 
+////////////////////////////////////////////////////////////
+
+require '../scriptsphp/defenvvar.php';
+
+$event=Array();
+$sportIds=null;
+if(isset($_POST['sportIds'])){
+$sportIds = $_POST["sportIds"];
+$sportIdArray = json_decode($sportIds);
+}
+
+////////////////////////////////////////////////////////////
+//
+// 	Connections � la base de donn�es
+//
+////////////////////////////////////////////////////////////
+
+// Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn, "SET NAMES 'utf8'");
+mysqli_query($conn, "SET CHARACTER SET 'utf8'");
+
+$saisonId =null;
+
+/////////////////////////////////////////////////////////////
+// 
+//
+
+if(!empty($sportIds)){
+	$rfEventType = mysqli_query($conn,"SELECT * FROM EventType WHERE 1")
+	or die(mysqli_error($conn)." Select EventType Sport null"); 
+}else{
+	$sportString = "";
+	foreach($s as $sportIdArray){
+		$sportString  = $sportString . "SportID = '{$s}' OR ";
+	}
+	if(strlen($sportString)>4){
+		$sportString= substr($sportString, 0, -3);
+	}
+	
+
+
+	$rfEventType = mysqli_query($conn,"SELECT * FROM EventType WHERE {$sportString} ")
+	or die(mysqli_error($conn)." Select EventType Sport set to {$sportIds} "); 
+}
+while($rangeeEvent=mysqli_fetch_assoc($rfEventType))
+{
+	$rangeeEvent['CreatedAt'] = strtotime($rangeeEvent['CreatedAt']); // convert to unix timestamp (in seconds)
+    $rangeeEvent['CreatedAt'] = 1000 * $rangeeEvent['CreatedAt']; // convert seconds to milliseconds
+	$rangeeEvent['UpdateAt'] = strtotime($rangeeEvent['UpdateAt']); // convert to unix timestamp (in seconds)
+    $rangeeEvent['UpdateAt'] = 1000 * $rangeeEvent['UpdateAt']; // convert seconds to milliseconds
+	$event[] = $rangeeEvent;
+	
+}
+	
+echo json_encode($event);;
+	
+mysqli_close($conn);
+
+?>
