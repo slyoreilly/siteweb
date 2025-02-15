a6e2dc153e75c7856bd74a6cd47f75517f229ca8
Sylvain O'Reilly <soreilly@syncstats.com>
2025-02-15 09:46:52 -0500

    création api/upsertMatch

---
+++ api/upsertMatch.php
@@ -0,0 +1,91 @@
+<?php
+////////////////////////////////////////////////////////////
+//
+//	upsertMatch.php
+//	Est appelé dans MatchRepository.kt
+//
+//
+////////////////////////////////////////////////////////////
+
+include_once ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR . "syncstatsconfig.php");
+
+require '../scriptsphp/defenvvar.php';
+// Create connection
+$conn = mysqli_connect($db_host, $db_user, $db_pwd, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+mysqli_query($conn,"SET NAMES 'utf8'");
+mysqli_query($conn,"SET CHARACTER SET 'utf8'");
+	
+
+$preMatch =null;
+if(isset($_POST['match'])){
+	$preMatch = $_POST["match"];
+	$match = json_decode($preMatch, true);
+	}
+	
+
+
+$heure = $_POST['heure'];
+$heureServeur = time()*1000;
+
+$syncOK = array();
+
+if($match != null) {
+
+
+		if (isset($heure)) {
+			// retourner le but, sans correction de chrono.
+			//$unClip['chrono'] = $unClip['chrono'] + $heureServeur - $heure;
+		}
+
+
+		if($match['GameComId']<1){
+			$qInsM = "INSERT INTO TableMatch (eq_dom, score_dom, eq_vis, score_vis, statut, matchIdRef, ligueRef, date, cleValeur, arenaId, TSDMAJ) 
+			VALUES ('{$match['eqDom']}','{$match['scoreDom']}','{$match['eqVis']}','{$match['scoreVis']}',0,'{$match['matchLongId']}','{$match['ligueId']}','{$match['date']}','{$match['cleValeur']}','{$match['arenaId']}','{$match['dernierMAJ']}')";
+
+			mysqli_query($conn,$qInsM) or die(mysqli_error($conn) . $qInsM);
+			$webMatchId=mysqli_insert_id($conn);
+
+		}
+
+		else{
+			$retour = mysqli_query($conn,"UPDATE TableMatch 
+			SET eq_dom='{$match['eqDom']}',
+			score_dom='{$match['scoreDom']}',
+			eq_vis='{$match['eqVis']}',
+			score_vis='{$match['scoreVis']}',
+			statut='{$match['etat']}',
+			matchIdRef='{$match['matchLongId']}',
+			ligueRef='{$match['ligueId']}',
+			date='{$match['date']}',
+			cleValeur='{$match['cleValeur']}',
+			arenaId='{$match['eqDom']}',
+			TSDMAJ=NOW() 
+			WHERE match_id='{$match['GameComId']}'");	
+
+			$webMatchId = $match['GameComId']
+
+		}
+
+			
+
+		
+
+				
+
+				
+				$retObj = array("id"=>$webMatchId);
+				array_push($syncOK, $retObj);
+		
+	}
+
+echo json_encode($syncOK);
+
+
+mysqli_close($conn);
+
+?>
