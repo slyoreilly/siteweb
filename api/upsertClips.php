b57e244bf13eebfe7cf857389fe4d5fdb3d41fed
Sylvain O'Reilly <soreilly@syncstats.com>
2026-01-27 15:02:00 -0500

    Corr dans unpsertClips

--- api/upsertClips.php
+++ api/upsertClips.php
@@ -3,7 +3,46 @@
 include_once ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR . "syncstatsconfig.php");
 
 require '../scriptsphp/defenvvar.php';
-require_once 'creeMatchDeClip.php';
+
+function creerMatchSiInexistant($conn, $matchClientId, $plateauId, $ligueId) {
+    $now = date("Y-m-d H:i:s");
+    $tsdm = round(microtime(true) * 1000);
+
+    $qCheck = "SELECT * FROM TableMatch WHERE matchIdRef = '$matchClientId'";
+    $result = mysqli_query($conn, $qCheck);
+    if (mysqli_num_rows($result) > 0) {
+        return null; // Match existe déjà
+    }
+
+    $qEquipes = "SELECT equipeId FROM TableEquipe WHERE ligueId = $ligueId LIMIT 2";
+    $resEquipes = mysqli_query($conn, $qEquipes);
+
+    if (mysqli_num_rows($resEquipes) < 2) {
+        throw new Exception("Pas assez d'équipes dans la ligue $ligueId");
+    }
+
+    $equipes = [];
+    while ($row = mysqli_fetch_assoc($resEquipes)) {
+        $equipes[] = intval($row['equipeId']);
+    }
+
+    $eqDom = $equipes[0];
+    $eqVis = $equipes[1];
+
+    $qInsert = "INSERT INTO TableMatch (
+        eq_dom, score_dom, eq_vis, score_vis, statut, matchIdRef,
+        ligueRef, date, TSDMAJ, arenaId
+    ) VALUES (
+        $eqDom, 0, $eqVis, 0, 'F', '$matchClientId',
+        $ligueId, '$now', $tsdm, $plateauId
+    )";
+
+    if (!mysqli_query($conn, $qInsert)) {
+        throw new Exception("Erreur MySQL : " . mysqli_error($conn));
+    }
+
+    return mysqli_insert_id($conn);
+}
 
 
 $preClips =null;
@@ -18,6 +57,7 @@ $heure = $_POST['heure'];
 $heureServeur = time()*1000;
 
 $syncOK = array();
+
 
 foreach ($clips as $unClip) {
 
@@ -42,7 +82,7 @@ foreach ($clips as $unClip) {
 
 				if(isset($unClip['plateauId'])&&isset($unClip['ligueId']))
 				{
-					creerMatchSiInexistant($conn, $unClip['GameStringID'], $plateauId, $ligueId);
+					creerMatchSiInexistant($conn, $unClip['GameStringID'],  $unClip['plateauId'], $unClip['ligueId']);
 				}
 	}
 
